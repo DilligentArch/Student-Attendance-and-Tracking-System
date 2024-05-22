e776e5f3472e2b4ba89bd530218729a18448eaa3
shafichy <shafichy1400@gmail.com>
2024-05-22 20:45:24 +0600

    added a login.js file

---
+++ js/login.js
@@ -0,0 +1,62 @@
+login.js
+function tryLogin()
+{
+    let un=$("#txtUsername").val();
+    let pw=$("#txtPassword").val();
+    if(un.trim()!=="" && pw.trim()!="")
+    {
+      
+       $.ajax({
+        url:"ajaxhandler/loginAjax.php",
+        type:"POST",
+        dataType:"json",
+        data:{user_name:un,password:pw,action:"verifyUser"},
+        beforeSend:function(){
+           
+            $("#diverror").removeClass("applyerrordiv");
+            $("#lockscreen").addClass("applylockscreen");
+            
+            
+        },
+        success:function(rv){
+            
+            $("#lockscreen").removeClass("applylockscreen");
+            if(rv['status']=="ALL OK")
+            {
+                document.location.replace("attendance.php");
+            }
+            else{
+               
+                $("#diverror").addClass("applyerrordiv");
+                $("#errormessage").text(rv['status']);
+            }
+        },
+        error:function(){
+            
+            alert("oops something went wrong");
+        },
+       });
+    }
+}
+
+$(function(e){
+    
+    $(document).on("keyup","input",function(e){
+        $("#diverror").removeClass("applyerrordiv");
+        //$("#errormessage").text("");
+        let un=$("#txtUsername").val();
+        let pw=$("#txtPassword").val();
+        if(un.trim()!=="" && pw.trim()!=="")
+        {
+          $("#btnLogin").removeClass("inactivecolor");
+          $("#btnLogin").addClass("activecolor");
+        }
+        else{
+            $("#btnLogin").removeClass("activecolor");
+            $("#btnLogin").addClass("inactivecolor");
+        }
+    }); 
+    $(document).on("click","#btnLogin",function(e){
+        tryLogin();
+    });
+});
