4903dd97cfc49bfb68c93b0b7b14b328e40d8f8b
Raisa Ahmed <106526901+Raisaxleex@users.noreply.github.com>
2024-05-22 20:32:24 +0600

    Created an about us file

---
+++ about.php
@@ -0,0 +1,138 @@
+85% of storage used … If you run out, you can't create, edit and upload files. Get 100 GB of storage for BDT 150.00 BDT 0 for 1 month.
+about.php
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body  class="bg-[#83c5be]">
+       
+  <Navbar>
+    <div class="container mx-auto">
+     <div class="navbar bg-slate-700">
+         <div class="navbar-start">
+           <div class="dropdown">
+             <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+             </div>
+             <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+               <li >
+                 <a  href="/attendanceapp/index.php">Home</a>
+             </li>
+           
+               <li>
+                 <a  href="/attendanceapp/about.php">About Us</a>
+               </li>
+               <li>
+                 <a  href="/attendanceapp/attendance.php">Attendance</a>
+               </li>
+              
+             </ul>
+           </div>
+           <a class="btn btn-ghost text-xl text-white">MetroPolitan</a>
+         </div>
+         <div class="navbar-center hidden lg:flex">
+           <ul class="menu menu-horizontal px-1">
+             <li>
+                 <a class="text-white" href="/attendanceapp/index.php">Home</a>
+             </li>
+           
+             <li>
+                 <a class="text-white" href="/attendanceapp/about.php">About Us</a>
+             </li>
+             
+             
+             <li>
+                 <a class="text-white" href="/attendanceapp/attendance.php">Attendance</a>
+               </li>
+           </ul>
+         </div>
+         <div class="navbar-end flex gp-4">
+         <button class="px-3 py-3 w-[80px] bg-white text-black rounded-xl "> <a href="/attendanceapp/login.php" class="">Login</a></button>
+         
+         
+          
+         </div>
+       </div>
+    </div>
+  
+    
+ </Navbar>
+   <h1 class="container mx-auto mt-10 text-[#111111] font-semibold  text-base lg:text-xl text-center leading-10 w-[350px] lg:w-[1500px]">  Welcome to the dedicated team behind <span class="text-[#F78C9C]">"AttendEase: </span>Simplifying Attendance Management". We are a group of passionate and driven individuals  brought together <br> by our shared enthusiasm for revolutionizing the way attendance is managed in educational institutions.  Our project aims to streamline <br> attendance tracking processes, promote transparency, and enhance overall efficiency in educational settings</h1>
+
+   <h1 class="mt-10 text-center text-[20px] lg:text-[40px] bg-[#403D58] w-[200px] lg:w-[400px] mx-auto p-2 text-white rounded-md">Meet Our Team</h1>
+
+   <div class="container mx-auto mt-20 ">
+      <div class="flex flex-col lg:flex-row gap-[150px] lg:ml-[200px]">
+        <div class="bg-[#1313130D] p-8 rounded-xl w-[400px]">
+          <img class="w-[350px] rounded-xl" src="Anika.png" alt="">
+          <h1 class="mt-6 text-[#131313] font-bold">Name  : Nurun Nahar Anika</h1>
+          <h1 class="text-[#131313] font-semibold">ID  :   221-115-095</h1>
+    </div>
+        <div class="bg-[#1313130D] p-8 rounded-xl w-[400px]">
+          <img class="w-[350px] rounded-xl " src="nayeb2.jpg" alt="">
+          <h1 class="mt-6 text-[#131313] font-bold">Name  : Nayeb Ahmed Querishi</h1>
+          <h1 class="text-[#131313] font-semibold">ID  :   221-115-097</h1>
+    </div>
+      </div>
+      <div class="flex flex-col lg:flex-row gap-[150px] lg:ml-[200px] mt-10">
+        <div class="bg-[#1313130D] p-8 rounded-xl w-[400px]">
+          <img class="w-[350px] rounded-xl" src="shafi.jpeg" alt="">
+          <h1 class="mt-6 text-[#131313] font-bold">Name  : Shafi Chowdhury</h1>
+          <h1 class="text-[#131313] font-semibold">ID  :   221-115-118</h1>
+    </div>
+        <div class="bg-[#1313130D] p-8 rounded-xl w-[400px]">
+          <img class="w-[350px] rounded-xl " src="raisa.png" alt="">
+          <h1 class="mt-6 text-[#131313] font-bold">Name  : Raisa Ahmed</h1>
+          <h1 class="text-[#131313] font-semibold">ID  :   221-115-124</h1>
+    </div>
+      </div>
+   </div>
+   <Footer class="mt-10 bg-[#030712]">
+      
+    <div class="container">
+      <div class="p-10">
+        <h1 class="text-white text-center lg:ml-[370px]">Attendance Management System</h1>
+        <div class="col-md-4 lg:ml-[750px] leading-10 text-white">
+          <h5 class="text-white ml-[130px]">Contact Us</h5>
+          <p class="ml-[60px]">Email: shafichy1400@gmail.com</p>
+        
+        
+        </div>
+        <div class="col-md-4 text-center  text-white lg:ml-[330px] mt-10">
+          <h5 class="lg:ml-[50px]">Quick Links</h5>
+          <ul class="flex gap-6 ml-5 lg:ml-[450px] mt-5">
+            <li><a href="/attendanceapp/index.php">Home</a></li>
+            <li><a href="/attendanceapp/about.php">About Us</a></li>
+            <li><a href="/attendanceapp/login.php">Login</a></li>
+            <li><a href="/register">Register</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4 text-center lg:ml-[300px] mt-8 text-white">
+          <h5 class=" lg:ml-[90px]">Connect With Us</h5>
+          <ul class="social-media flex ml-[100px] lg:ml-[570px] gap-8 mt-4">
+            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <hr class="ml-4 lg:ml-[500px] w-[350px] lg:w-[900px]">
+      <div class="row p-8">
+        <div class="col-md-6 text-white text-center  lg:ml-[400px]">
+          <p>&copy; 2024 AttendEase. All Rights Reserved.</p>
+        </div>
+        <div class="col-md-6 text-white text-center lg:ml-[400px] mt-3">
+          <p>Terms of Service | Privacy Policy</p>
+        </div>
+      </div>
+    </div>
+
+  
+</Footer>
+</body>
+</html>
